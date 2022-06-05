dea7e88e4175a3c69a08eae0e31d0ead5fbf9940
nubia <nubiapontel@outlook.com>
2022-06-05 15:43:20 -0300

    dashboard

--- src/views/Pedidos.vue
+++ src/views/Pedidos.vue
@@ -1,5 +1,18 @@
 <template>
   <div class="main-container">
-    <h1>pedidos</h1>
+    <h1>Gerenciar pedidos:</h1>
+    <Dashboard />
   </div>
 </template>
+
+
+<script>
+import Dashboard from "../components/Dashboard.vue"
+
+export default {
+  name: "pedidos",
+  components:{
+    Dashboard
+  } 
+}
+</script>
