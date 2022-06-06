79c347f11a1ac518594d579020881db02dd9778b
nubia <nubiapontel@outlook.com>
2022-06-06 15:48:55 -0300

    pedidos do banco

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -12,15 +12,16 @@
       </div>
 
       <div id="burger-table-rows">
-          <div class="burger-table-row">
-              <div class="order-number">1</div>
-              <div>Joao</div>
-              <div>Pao de trigo</div>
-              <div>Maminha</div>
+          <div class="burger-table-row" v-for="burger in burgers" :key="burger.id">
+              <div class="order-number">{{ burger.id }}</div>
+              <div>{{ burger.nome }}</div>
+              <div>{{burger.pao}}</div>
+              <div>{{burger.carne}}</div>
               <div>
                   <ul>
-                      <li>salame</li>
-                      <li>tomate</li>
+                      <li v-for="(opcional, index) in burger.opcionais" :key="index">
+                        {{opcional}}</li>
+                      
                   </ul>
               </div>
               <div>
@@ -36,7 +37,29 @@
 
 <script>
 export default {
-   name: "Dashboard" 
+   name: "Dashboard",
+   data(){
+     return{
+       burgers: null,
+       burger_id: null,
+       status: []
+     }
+   },
+   methods: {
+     async getPedidos(){
+
+       const req = await fetch("http://localhost:3000/burgers");
+
+       const data = await req.json();
+
+       this.burgers = data
+
+       //resgatar o status
+     }
+   },
+   mounted() {
+     this.getPedidos();
+   }
 }
 </script>
 
