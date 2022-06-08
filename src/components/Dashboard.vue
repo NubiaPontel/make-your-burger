33858593e4b8a55a22e7cb7607ec3c619b005f40
nubia <nubiapontel@outlook.com>
2022-06-08 00:09:43 -0300

    status do pedido

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -27,6 +27,8 @@
               <div>
                   <select name="status" class="status">
                       <option value="">Selecione</option>
+                      <option v-for="s in status" :key="s.id" value="s.tipo" :selected="burger.status == s.tipo">
+                        {{ s.tipo }}</option>
                   </select>
                   <button class="delete-btn">Cancelar</button>
               </div>
@@ -55,6 +57,16 @@ export default {
        this.burgers = data
 
        //resgatar o status
+       this.getStatus()
+     },
+
+     async getStatus(){
+
+       const req = await fetch("http://localhost:3000/status");
+       const data = await req.json();
+
+       this.status = data;
+
      }
    },
    mounted() {
