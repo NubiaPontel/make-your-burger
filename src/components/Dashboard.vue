8f0d37c74e7ac646cb662cc61cf87f398cb96247
nubia <nubiapontel@outlook.com>
2022-06-14 23:27:01 -0300

    removendo pedidos

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -30,7 +30,7 @@
                       <option v-for="s in status" :key="s.id" value="s.tipo" :selected="burger.status == s.tipo">
                         {{ s.tipo }}</option>
                   </select>
-                  <button class="delete-btn">Cancelar</button>
+                  <button class="delete-btn" @click="deleteBurger(burger.id)">Cancelar</button>
               </div>
           </div>
       </div>
@@ -67,6 +67,16 @@ export default {
 
        this.status = data;
 
+     },
+
+     async deleteBurger(id){
+      const req = await fetch(`http://localhost:3000/burgers/${id}`, {
+        method: "DELETE"
+      });
+
+      const res = await req.json();
+
+      this.getPedidos()
      }
    },
    mounted() {
